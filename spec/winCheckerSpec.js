c0340bbe03f1cb0761f5e7027c53e7148c2a2f64
marcusfgardiner <marcusfgardiner@gmail.com>
2018-07-11 10:50:35 +0100

    :white_check_mark: test: Setting up winChecker testing scenarios and files

---
+++ spec/winCheckerSpec.js
@@ -0,0 +1,19 @@
+describe("WinChecker", function() {
+    var winChecker;
+
+    beforeEach(function() {
+        winChecker = new WinChecker
+        winCombo1 = [["x", "x", "x"], ["-", "-", "-"], ["-", "-", "-"]];
+        winCombo2 = [["-", "-", "-"], ["x", "x", "x"], ["-", "-", "-"]];
+        winCombo3 = [["-", "-", "-"], ["-", "-", "-"], ["x", "x", "x"]];
+        winCombo4 = [["x", "-", "-"], ["x", "-", "-"], ["x", "-", "-"]];
+        winCombo5 = [["-", "x", "-"], ["-", "x", "-"], ["-", "x", "-"]];
+        winCombo6 = [["-", "-", "x"], ["-", "-", "x"], ["-", "-", "x"]];
+        winCombo7 = [["x", "-", "-"], ["-", "x", "-"], ["-", "-", "x"]];
+        winCombo8 = [["-", "-", "x"], ["-", "x", "-"], ["x", "-", "-"]];
+    });
+
+    it("can correctly assess a win from 3 x's in top row", function() {
+
+    });
+});

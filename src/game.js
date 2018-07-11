6d109052341c7de24f8403d41a873d2b59393d24
marcusfgardiner <marcusfgardiner@gmail.com>
2018-07-11 10:20:17 +0100

    :sparkles: feature: can conduct a player move to update the board

--- src/game.js
+++ src/game.js
@@ -5,3 +5,7 @@ var Game = function() {
 Game.prototype.readBoard = function(xCoordinate, yCoordinate) {
     return this.board[xCoordinate - 1][yCoordinate - 1]
 };
+
+Game.prototype.move = function(xCoordinate, yCoordinate, XorO) {
+    this.board[xCoordinate - 1][yCoordinate - 1] = XorO
+};

6d738c2c3eb3fdeb5fd41f08325771fa81241ff7
marcusfgardiner <marcusfgardiner@gmail.com>
2018-07-11 10:14:47 +0100

    :white_check_mark: test: function updates board pased on player move

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -13,4 +13,9 @@ describe("Game", function() {
         game.board = [["-", "-", "-"], ["-", "x", "-"], ["-", "-", "-"]];
         expect(game.readBoard(2,2)).toEqual('x')
     });
+
+    it("can update the board based on a provided string input", function() {
+        game.move(1,2,'x')
+        expect(game.readBoard(1,2)).toEqual("x");
+    });
 });

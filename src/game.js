d384454bae870196261b3334bb524e50552f582c
marcusfgardiner <marcusfgardiner@gmail.com>
2018-07-11 09:52:49 +0100

    :white_check_mark: test: function to read value from the board

--- src/game.js
+++ src/game.js
@@ -2,6 +2,6 @@ var Game = function() {
     this.board = ["-", "-", "-", "-", "-", "-", "-", "-", "-"];
 };
 
-Game.prototype.readBoard = function(xCoordinate, yCoordinate) {
+// Game.prototype.readBoard = function(xCoordinate, yCoordinate) {
     
-};
+// };

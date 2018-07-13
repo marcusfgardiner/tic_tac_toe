cd483423103708b02a1c5531afa6b5695822a9de
marcusfgardiner <marcusfgardiner@gmail.com>
2018-07-13 15:51:26 +0100

    :recycle: :hankey: refactor: Cleanup begun - need two separate functions because checking multiple win directions, but need to work out which code sits in either function and how to better test both functions

--- src/winChecker.js
+++ src/winChecker.js
@@ -7,29 +7,20 @@ WinChecker.prototype.isWinningMove = function(board, XorO) {
     // Will probably need a recursion/ loop/ function for each of these 4 check types
     // Work one by one through them and refactor later
     // TODO: current task: ALL horizontals covered through recursion (or loop)
-    console.log('function result',(this.isNextTileSame(board, 0, 0, "horizontal")));
     return (this.isNextTileSame(board, 0, 0, 'horizontal'))
-    // return ((board[0][0] === XorO) && (board[0][1] === XorO) && (board[0][2] === XorO))
 };
 
 WinChecker.prototype.isNextTileSame = function(board, rowNumber, cellNumber, movement) {
     var currentTile = board[rowNumber][cellNumber]
     var nextTile = board[rowNumber][cellNumber + 1]
-    console.log("current tile: ", currentTile);
-    console.log('next tile: ', nextTile)
-    // var returnValue;
     if (nextTile === undefined) {
         console.log('undefined tree')
         return true
-        // returnValue = true;
-        // console.log("return value inside if scope", returnValue);
     } else if (currentTile === nextTile) {
         console.log("same tree");
         return this.isNextTileSame(board, rowNumber, cellNumber + 1, movement);
     } else {
         console.log("NOT same tree");
         return false
-        // returnValue = false;
-        // console.log("return value inside if scope", returnValue);
     }
 };

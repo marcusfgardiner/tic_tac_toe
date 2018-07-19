530af083d4b55d51a29a9cf520d68f2458c46a09
marcusfgardiner <marcusfgardiner@gmail.com>
2018-07-19 14:40:00 +0100

    :recycle: refactor: Improved testing statement descriptions

--- spec/winCheckerSpec.js
+++ spec/winCheckerSpec.js
@@ -18,47 +18,47 @@ describe("WinChecker", function() {
 
     describe("isWinningMove" ,function() {
         // TODO: add tests for ALL winning combos above (can copy function and just change subbed in win combo)
-        it("can correctly assess a win from 3 x's in top row", function() {
+        it("1 can correctly assess a win from 3 x's in top row", function() {
             expect(winChecker.isWinningMove(winCombo1, "x")).toBe(true);
         });
 
-        it("can correctly assess a win from 3 x's in second row", function() {
+        it("2 can correctly assess a win from 3 x's in second row", function() {
             expect(winChecker.isWinningMove(winCombo2, "x")).toBe(true);
         });
 
-        it("can correctly assess a win from 3 x's in third row", function() {
+        it("3 can correctly assess a win from 3 x's in third row", function() {
             expect(winChecker.isWinningMove(winCombo3, "x")).toBe(true);
         });
 
-        it("can correctly assess a win from 3 x's in top row", function () {
+        it("4 can correctly assess a win from 3 x's in first column", function () {
             expect(winChecker.isWinningMove(winCombo4, "x")).toBe(true);
         });
 
-        it("can correctly assess a win from 3 x's in second row", function () {
+        it("5 can correctly assess a win from 3 x's in second column", function () {
             expect(winChecker.isWinningMove(winCombo5, "x")).toBe(true);
         });
 
-        it("can correctly assess a win from 3 x's in third row", function () {
+        it("6 can correctly assess a win from 3 x's in third column", function () {
             expect(winChecker.isWinningMove(winCombo6, "x")).toBe(true);
         });
 
-        it("can correctly assess a win from 3 x's in top row", function () {
+        it("7 can correctly assess a win from 3 x's diagonal left to right", function () {
             expect(winChecker.isWinningMove(winCombo7, "x")).toBe(true);
         });
 
-        it("can correctly assess a win from 3 x's in second row", function () {
+        it("8 can correctly assess a win from 3 x's diagonal right to left", function () {
             expect(winChecker.isWinningMove(winCombo8, "x")).toBe(true);
         });
 
-        it("can correctly assess a non-win", function() {
+        it("1 can correctly assess a non-win", function() {
             expect(winChecker.isWinningMove(loseCombo1, "x")).toBe(false);
         });
 
-        it("can correctly assess a non-win", function () {
+        it("2 can correctly assess a non-win - no moves yet", function () {
             expect(winChecker.isWinningMove(loseCombo2, "x")).toBe(false);
         });
 
-        it("can correctly assess a non-win", function () {
+        it("3 can correctly assess a non-win - random x's and o's ", function () {
             expect(winChecker.isWinningMove(loseCombo3, "x")).toBe(false);
         });
     });

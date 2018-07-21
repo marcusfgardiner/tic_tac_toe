f8228639697fa8eda6c66a0da4ebea3fca6d04db
marcusfgardiner <marcusfgardiner@gmail.com>
2018-07-21 11:23:39 +0100

    :recycle: refactor: Indicate private functions

--- src/winChecker.js
+++ src/winChecker.js
@@ -4,51 +4,51 @@ var WinChecker = function(board, XorO) {
 }
 
 WinChecker.prototype.isWinningMove = function () {
-    if (this.isHorizontalWin()) {
+    if (this._isHorizontalWin()) {
         return true
     };
-    if (this.isVerticalWin()) {
+    if (this._isVerticalWin()) {
         return true
     };
-    if (this.isDiagonalRightWin()) {
+    if (this._isDiagonalRightWin()) {
         return true
     };
-    if (this.isDiagonalLeftWin()) {
+    if (this._isDiagonalLeftWin()) {
         return true
     };
     return false
 };
 
-WinChecker.prototype.isHorizontalWin = function () {
+WinChecker.prototype._isHorizontalWin = function () {
     for (let index = 0; index < this.board.length; index++) {
-        if(this.isWinningCombo(index, 0, "horizontal")) {
+        if(this._isWinningCombo(index, 0, "horizontal")) {
             return true
         };
     };
 };
 
-WinChecker.prototype.isVerticalWin = function () {
+WinChecker.prototype._isVerticalWin = function () {
     for (let index = 0; index < this.board.length; index++) {
-        if(this.isWinningCombo(0, index, "vertical")) {
+        if(this._isWinningCombo(0, index, "vertical")) {
             return true
         };
     };
 };
 
-WinChecker.prototype.isDiagonalRightWin = function () {
-    return this.isWinningCombo(0, 0, "diagonalRight")
+WinChecker.prototype._isDiagonalRightWin = function () {
+    return this._isWinningCombo(0, 0, "diagonalRight")
 };
 
-WinChecker.prototype.isDiagonalLeftWin = function () {
-    return this.isWinningCombo(0, (this.board.length - 1), "diagonalLeft")
+WinChecker.prototype._isDiagonalLeftWin = function () {
+    return this._isWinningCombo(0, (this.board.length - 1), "diagonalLeft")
 };
 
-WinChecker.prototype.isWinningCombo = function(rowNumber, cellNumber, movement) {
-    var currentTile = this.returnCurrentTile(rowNumber, cellNumber);
+WinChecker.prototype._isWinningCombo = function(rowNumber, cellNumber, movement) {
+    var currentTile = this._returnCurrentTile(rowNumber, cellNumber);
     // Current tile same as previous x/o tile => could still be a winning combination
     if (currentTile === this.XorO) {
-        ({ rowNumber, cellNumber } = this.tileChange(rowNumber, cellNumber, movement));
-        return this.isWinningCombo(rowNumber, cellNumber, movement);
+        ({ rowNumber, cellNumber } = this._tileChange(rowNumber, cellNumber, movement));
+        return this._isWinningCombo(rowNumber, cellNumber, movement);
     // End of board reached with only same tiles all the way along => winning combination
     } else if (currentTile === undefined) {
         return true
@@ -58,7 +58,7 @@ WinChecker.prototype.isWinningCombo = function(rowNumber, cellNumber, movement) 
     }
 };
 
-WinChecker.prototype.tileChange = function (rowNumber, cellNumber, movement) {
+WinChecker.prototype._tileChange = function (rowNumber, cellNumber, movement) {
     if (movement === 'horizontal' || movement === 'diagonalRight') {
         cellNumber++;
     }
@@ -71,7 +71,7 @@ WinChecker.prototype.tileChange = function (rowNumber, cellNumber, movement) {
     ;
     return { rowNumber, cellNumber };
 }
-WinChecker.prototype.returnCurrentTile = function (rowNumber, cellNumber) {
+WinChecker.prototype._returnCurrentTile = function (rowNumber, cellNumber) {
     var currentRow = this.board[rowNumber];
     if (currentRow !== undefined) {
         var currentTile = currentRow[cellNumber];

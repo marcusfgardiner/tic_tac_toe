75bbfe52db0ef5c027e5ddcd970021966670c2b8
marcusfgardiner <marcusfgardiner@gmail.com>
2018-07-21 10:59:12 +0100

    :recycle: :hankey: refactor: Refactor attempt to extract XorO and board results in error where isWinningMove returns true every time - issue unfixed

--- spec/winCheckerSpec.js
+++ spec/winCheckerSpec.js
@@ -2,7 +2,6 @@ describe("WinChecker", function() {
     var winChecker;
 
     beforeEach(function() {
-        winChecker = new WinChecker();
         winCombo1 = [["x", "x", "x"], ["-", "-", "-"], ["-", "-", "-"]];
         winCombo2 = [["-", "-", "-"], ["x", "x", "x"], ["-", "-", "-"]];
         winCombo3 = [["-", "-", "-"], ["-", "-", "-"], ["x", "x", "x"]];
@@ -18,73 +17,58 @@ describe("WinChecker", function() {
 
     describe("isWinningMove" ,function() {
         it("1 can correctly assess a win from 3 x's in top row", function() {
-            expect(winChecker.isWinningMove(winCombo1, "x")).toBe(true);
+            winChecker = new WinChecker(winCombo1, "x");
+            expect(winChecker.isWinningMove()).toBe(true);
         });
 
         it("2 can correctly assess a win from 3 x's in second row", function() {
-            expect(winChecker.isWinningMove(winCombo2, "x")).toBe(true);
+            winChecker = new WinChecker(winCombo2, "x");
+            expect(winChecker.isWinningMove()).toBe(true);
         });
 
-        it("3 can correctly assess a win from 3 x's in third row", function() {
-            expect(winChecker.isWinningMove(winCombo3, "x")).toBe(true);
+        it("3 can correctly assess a win from 3 x's in th   ird row", function() {
+            winChecker = new WinChecker(winCombo3, "x");
+            expect(winChecker.isWinningMove()).toBe(true);
         });
 
         it("4 can correctly assess a win from 3 x's in first column", function () {
-            expect(winChecker.isWinningMove(winCombo4, "x")).toBe(true);
+            winChecker = new WinChecker(winCombo4, "x");
+            expect(winChecker.isWinningMove()).toBe(true);
         });
 
         it("5 can correctly assess a win from 3 x's in second column", function () {
-            expect(winChecker.isWinningMove(winCombo5, "x")).toBe(true);
+            winChecker = new WinChecker(winCombo5, "x");
+            expect(winChecker.isWinningMove()).toBe(true);
         });
 
         it("6 can correctly assess a win from 3 x's in third column", function () {
-            expect(winChecker.isWinningMove(winCombo6, "x")).toBe(true);
+            winChecker = new WinChecker(winCombo6, "x");
+            expect(winChecker.isWinningMove()).toBe(true);
         });
 
         it("7 can correctly assess a win from 3 x's diagonal left to right", function () {
-            expect(winChecker.isWinningMove(winCombo7, "x")).toBe(true);
+            winChecker = new WinChecker(winCombo7, "x");
+            expect(winChecker.isWinningMove()).toBe(true);
         });
 
         it("8 can correctly assess a win from 3 x's diagonal right to left", function () {
-            expect(winChecker.isWinningMove(winCombo8, "x")).toBe(true);
+            winChecker = new WinChecker(winCombo8, "x");
+            expect(winChecker.isWinningMove()).toBe(true);
         });
 
         it("1 can correctly assess a non-win", function() {
-            expect(winChecker.isWinningMove(loseCombo1, "x")).toBe(false);
+            winChecker = new WinChecker(loseCombo1, "x");
+            expect(winChecker.isWinningMove()).toBe(false);
         });
 
         it("2 can correctly assess a non-win - no moves yet", function () {
-            expect(winChecker.isWinningMove(loseCombo2, "x")).toBe(false);
+            winChecker = new WinChecker(loseCombo2, "x");
+            expect(winChecker.isWinningMove()).toBe(false);
         });
 
         it("3 can correctly assess a non-win - random x's and o's ", function () {
-            expect(winChecker.isWinningMove(loseCombo3, "x")).toBe(false);
-        });
-    });
-
-    describe("isWinningCombo", function() {
-        it("can correctly assess a win from 3 x's in a row", function() {
-            var rowNumber = 0;
-            var cellNumber = 0;
-            var movement = "horizontal";
-            var result = winChecker.isWinningCombo(winCombo1, rowNumber, cellNumber, 'x', movement);
-            expect(result).toBe(true);
-        });
-
-        it("can correctly assess a NON-win from 2 x's in top row", function() {
-            var rowNumber = 0;
-            var cellNumber = 0;
-            var movement = "horizontal";
-            var result = winChecker.isWinningCombo(loseCombo1, rowNumber, cellNumber, "x", movement);
-            expect(result).toBe(false);
-        });
-
-        it("can correctly assess a NON-win from 3 -s in top row", function() {
-            var rowNumber = 0;
-            var cellNumber = 0;
-            var movement = "horizontal";
-            var result = winChecker.isWinningCombo(winCombo2, rowNumber, cellNumber, "x", movement);
-            expect(result).toBe(false);
+            winChecker = new WinChecker(loseCombo3, "x");
+            expect(winChecker.isWinningMove()).toBe(false);
         });
     });
 });
--- src/winChecker.js
+++ src/winChecker.js
@@ -1,63 +1,69 @@
-var WinChecker = function() {
-    //TODO: pass in board & XorO and save it to winChecker object on initiation = don't need to feed into methods every time
+// debugger; 
+
+var WinChecker = function(board, XorO) {
+    this.board = board
+    this.XorO = XorO
     //TODO: Rather than feeding in movement each time, store currentMovement on WinChecker object
 }
 
-WinChecker.prototype.isWinningMove = function(board, XorO) {
-    if (this.isHorizontalWin(board, XorO)) {
+WinChecker.prototype.isWinningMove = function () {
+    if (this.isHorizontalWin) {
         return true
     };
-    if (this.isVerticalWin(board, XorO)) {
+    if (this.isVerticalWin) {
         return true
     };
-    if (this.isDiagonalRightWin(board, XorO)) {
+    if (this.isDiagonalRightWin) {
         return true
     };
-    if (this.isDiagonalLeftWin(board, XorO)) {
+    if (this.isDiagonalLeftWin) {
         return true
     };
     return false
 };
 
-WinChecker.prototype.isHorizontalWin = function(board, XorO) {
-    for (let index = 0; index < board.length; index++) {
-        if(this.isWinningCombo(board, index, 0, XorO, "horizontal")) {
+WinChecker.prototype.isHorizontalWin = function () {
+            console.log("what is this", this);
+    // console.log('board length',this.board.length)
+    for (let index = 0; index < this.board.length; index++) {
+        console.log('YOYOYO')
+        if(this.isWinningCombo(index, 0, "horizontal")) {
             return true
         };
     };
 };
 
-WinChecker.prototype.isVerticalWin = function(board, XorO) {
-    for (let index = 0; index < board.length; index++) {
-        if(this.isWinningCombo(board, 0, index, XorO, "vertical")) {
+WinChecker.prototype.isVerticalWin = function () {
+    for (let index = 0; index < this.board.length; index++) {
+        if(this.isWinningCombo(0, index, "vertical")) {
             return true
         };
     };
 };
 
-WinChecker.prototype.isDiagonalRightWin = function(board, XorO) {
-    return this.isWinningCombo(board, 0, 0, XorO, "diagonalRight")
+WinChecker.prototype.isDiagonalRightWin = function () {
+    return this.isWinningCombo(0, 0, "diagonalRight")
 };
 
-WinChecker.prototype.isDiagonalLeftWin = function(board, XorO) {
-    return this.isWinningCombo(board, 0, (board.length - 1), XorO, "diagonalLeft")
+WinChecker.prototype.isDiagonalLeftWin = function () {
+    return this.isWinningCombo(0, (this.board.length - 1), "diagonalLeft")
 };
 
-WinChecker.prototype.isWinningCombo = function(board, rowNumber, cellNumber, XorO, movement) {
+WinChecker.prototype.isWinningCombo = function(rowNumber, cellNumber, movement) {
     console.log('row number', rowNumber)
     console.log('cell number', cellNumber)
     console.log('movement', movement)
-    console.log('board', board)
+    console.log('this.board', this.board)
     // Issue when trying to read rowNumber = 3 that broke test. Fixed with below if statement - need to refactor to remove
-    // Key issue: trying to read the cell number when board[rowNumber] is undefined => error
-    var currentRow = board[rowNumber]
+    // Key issue: trying to read the cell number when this.board[rowNumber] is undefined => error
+    var currentRow = this.board[rowNumber]
     if (currentRow !== undefined) {
         var currentTile = currentRow[cellNumber]
     }
-    console.log('XorO: ', XorO)
+    console.log('this.XorO: ', this.XorO)
     console.log('current tile:', currentTile)
     // Same value as previous tile => could still be a winning move
-    if (currentTile === XorO) {
+    if (currentTile === this.XorO) {
         console.log("good so far tree");
         // TODO: Refactor to extract this branch into its own function of tileChange
         if (movement === 'horizontal' || movement === 'diagonalRight') {
@@ -69,8 +75,8 @@ WinChecker.prototype.isWinningCombo = function(board, rowNumber, cellNumber, Xor
         if (movement === 'diagonalLeft') {
             cellNumber--
         };
-        return this.isWinningCombo(board, rowNumber, cellNumber, XorO, movement);
-    // End of board reached with only same tiles all the way along => winning move
+        return this.isWinningCombo(rowNumber, cellNumber, movement);
+    // End of this.board reached with only same tiles all the way along => winning move
     } else if (currentTile === undefined) {
         console.log('undefined tree')
         return true
